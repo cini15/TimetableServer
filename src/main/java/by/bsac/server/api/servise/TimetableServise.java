79ddcc757fd681f9fbc4352e3ab2a148e999dcfa
vlad <cini1994@gmail.com>
2018-05-03 23:03:53 +0300

    demo test

--- src/main/java/by/bsac/server/api/servise/TimetableServise.java
+++ src/main/java/by/bsac/server/api/servise/TimetableServise.java
@@ -3,8 +3,6 @@ package by.bsac.server.api.servise;
 import by.bsac.server.api.date.dto.ChairDTO;
 import by.bsac.server.api.date.dto.FacultyDTO;
 import by.bsac.server.api.date.dto.RecordDTO;
-import by.bsac.server.api.date.dto.SubjectDTO;
-import by.bsac.server.api.date.entity.Record;
 
 import java.util.List;
 
